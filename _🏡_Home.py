502697884d5b3734f1ad1775b0f6261ea87ffb5c
nguyen1872 <thanhnguyen187201@gmail.com>
2023-03-13 12:57:29 +0700

    adjust w

---
+++ _🏡_Home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from Resource_init import Login
+
+st.set_page_config(
+    page_title="Home Page",
+    page_icon="🟢",
+)
+
+
+if __name__ == '__main__':
+    if Login(w = True) == False:
+        st.stop()
+# st.markdown(
+#     """
+#     Streamlit is an open-source app framework built specifically for
+#     Machine Learning and Data Science projects.
+#     **👈 Select a demo from the sidebar** to see some examples
+#     of what Streamlit can do!
+#     ### Want to learn more?
+#     - Check out [streamlit.io](https://streamlit.io)
+#     - Jump into our [documentation](https://docs.streamlit.io)
+#     - Ask a question in our [community
+#         forums](https://discuss.streamlit.io)
+#     ### See more complex demos
+#     - Use a neural net to [analyze the Udacity Self-driving Car Image
+#         Dataset](https://github.com/streamlit/demo-self-driving)
+#     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+# """
+# )
